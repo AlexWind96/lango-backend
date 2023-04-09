4257a9f6932a84d52e5dc66ae04a33c910d640a0
Alex Pochebut <alexpochebut96@gmail.com>
2023-04-09 12:52:29 +0300

    feat: setup cors

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,10 @@ import helmet from 'helmet'
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
   app.use(cookieParser())
-  // app.use(helmet())
+  app.use(helmet())
   // app.enableCors({
   //   origin: '*',
   // })
